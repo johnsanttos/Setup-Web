9b59a27bcdc0a4cda99e62dc171c65f0831f9b0e
John Santos <jhow.mito11@gmail.com>
2023-04-05 19:13:49 -0300

    ok

--- setupWeb/src/App.tsx
+++ setupWeb/src/App.tsx
@@ -1,7 +1,7 @@
 //import './styles/global.css'
 import { Habit } from './components/Habit'
+import { Header } from './components/Hearder';
 import './index.css';
-import logoImage from './assets/logo.svg'
 
 function App() {
  return(
@@ -9,18 +9,7 @@ function App() {
   <div className=' w-screen h-screen flex justify-center items-center '>
   <div className='w-full max-w-5xl px-6 flex flex-col gap-16 '>
     
-    <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
-    <img src={logoImage} alt="habits" />
-
-    <button
-    type='button'
-    className='border border-violet-500 font-semibold rounded-lg px-6 py-4'
-    >
-      Novo hábito
-     </button>
-      
- 
-    </div>
+    <Header/>
  
   </div>
 </div>
