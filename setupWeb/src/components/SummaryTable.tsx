b3a1f321f6a84c7bafb6ead723631130211b7bfb
John Santos <jhow.mito11@gmail.com>
2023-04-10 19:39:46 -0300

    pagina inicial

--- setupWeb/src/components/SummaryTable.tsx
+++ setupWeb/src/components/SummaryTable.tsx
@@ -14,7 +14,11 @@ export default function SummaryTable() {
 
     const summaryDates = generateDatesFromYearBeginning()
 
-    console.log('la eleeee , ' , summaryDates )
+    const minimumSummaryDatesSize = 18 * 7 // 18 weeks
+
+    const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+
+    console.log('la eleeee , ', summaryDates)
     return (
         <div className="w-full flex ">
             <div className=" grid grid-rows-7 grid-flow-row gap-3">
@@ -25,38 +29,33 @@ export default function SummaryTable() {
                         return (
                             <div key={`${weekDays}-${i}`}
                                 className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center ">
-                                {
-                                    weekDays
-                                }
+                                {weekDays}
                             </div>
                         )
                     }
                     )
                 }
-               
+
             </div>
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
-<HabitDay/>
-<HabitDay/>
-<HabitDay/>
-<HabitDay/>
-<HabitDay/>
-<HabitDay/>
-<HabitDay/>
-<HabitDay/>
-<HabitDay/>
-<HabitDay/>
-<HabitDay/>
-<HabitDay/>
-<HabitDay/>
-<HabitDay/>
-<HabitDay/>
-<HabitDay/>
-<HabitDay/>
-<HabitDay/>
 
+                {
+                    summaryDates.map(date => {
+                        return <HabitDay key={date.toString()} />
+                    })
+                }
 
+                {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((_,i)=>{
+                    return(
+                     
+                        <div 
+                        key={i}
+                        className="bg-zinc-900 w-10 text-white rounded m-2  flex text-center items-center justify-center">
+                        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg  opacity-40 cursor-not-allowed" ></div>
+                            </div>
+                    )
+                })}
             </div>
 
         </div>
