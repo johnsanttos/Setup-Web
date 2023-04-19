01e03229552516106256232c37be9c22908aa44a
John Santos <jhow.mito11@gmail.com>
2023-04-19 12:04:19 -0300

    Component checked

--- setupWeb/src/components/NewHabitForm.tsx
+++ setupWeb/src/components/NewHabitForm.tsx
@@ -1,5 +1,5 @@
 import { Check } from "phosphor-react";
-
+import * as Checkbox from '@radix-ui/react-checkbox';
 
 
 export default function NewHabitForm() {
@@ -19,6 +19,22 @@ export default function NewHabitForm() {
             <label htmlFor="" className="font-semibold leading-tight mt-4">
                 Qual a recorrêcia?
             </label>
+
+            <div className="flex flex-col gap-2 mt-3">
+                <Checkbox.Root className='flex items-center gap-3 group'>
+
+                    <div className='h-8 w-8 rounded-xl flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+                        <Checkbox.Indicator>
+                            <Check size={20} className="text-white"/>
+                        </Checkbox.Indicator>
+                    </div>
+
+                    <span className='font-semibold text-xl text-white leading-tight  '>
+                        Segunda-feira
+                    </span>
+                </Checkbox.Root>
+
+            </div>
 
             <button type="submit"
                 className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500"
